3d6be5ed42c210d32f35d83c3995a305d2c40e07
Mohammad Dilshad <pzanpk123@gmail.com>
2025-07-03 11:50:06 +0530

    Added Hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-  import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import './App.css'
 import Home from './pages/Home.jsx'
@@ -13,12 +13,12 @@ import Contact from './pages/Contact.jsx'
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
        <Header />
        <Routes>
 
         <Route path='/'  element={<Home />}/>
-        <Route path='/about'  element={<About />}/>
+        <Route path='/About'  element={<About />}/>
         <Route path='/Academics'  element={<Academics />}/>
         <Route path='/Admissions'  element={<Admissions />}/>
         <Route path='/StudentLife'  element={<StudentLife />}/>
@@ -26,7 +26,7 @@ function App() {
        </Routes>
       
     <Footer />
-    </BrowserRouter>
+    </HashRouter>
     </div>
   )
 }
